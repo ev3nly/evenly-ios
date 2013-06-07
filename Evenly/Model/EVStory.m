7f4bb838202a721badb618e5f5c5ed8a408c2a74
Joe Hankin <joseph.hankin@gmail.com>
2013-06-06 17:39:44 -0700

    Created preliminary newsfeed API

---
+++ Evenly/Model/EVStory.m
@@ -0,0 +1,50 @@
+//
+//  EVStory.m
+//  Evenly
+//
+//  Created by Joseph Hankin on 6/6/13.
+//  Copyright (c) 2013 Evenly. All rights reserved.
+//
+
+#import "EVStory.h"
+#import "EVUser.h"
+#import "EVPayment.h"
+#import "EVCharge.h"
+
+@implementation EVStory
+
+- (void)setProperties:(NSDictionary *)properties {
+    [super setProperties:properties];
+    
+    // Easy things first
+    self.verb = properties[@"verb"];
+    self.isPrivate = [properties[@"private"] boolValue];
+    self.storyDescription = properties[@"description"];
+    if ([properties[@"amount"] isKindOfClass:[NSDecimalNumber class]])
+        self.amount = properties[@"amount"];
+    else
+        self.amount = [NSDecimalNumber decimalNumberWithString:properties[@"amount"]];
+    
+    // Subject
+    NSString *subjectClass = [NSString stringWithFormat:@"EV%@", properties[@"subject_type"]];
+    self.subject = [[NSClassFromString(subjectClass) alloc] init];
+    [self.subject setName:properties[@"subject_name"]];
+    [self.subject setDbid:properties[@"subject_id"]];
+    
+    // Target
+    if (properties[@"target_type"] != [NSNull null])
+    {
+        NSString *targetClass = [NSString stringWithFormat:@"EV%@", properties[@"target_type"]];
+        self.target = [[NSClassFromString(targetClass) alloc] init];
+        [self.target setName:properties[@"target_name"]];
+        [self.target setDbid:properties[@"target_id"]];
+    }
+    
+    // Owner
+    NSString *ownerClass = [NSString stringWithFormat:@"EV%@", properties[@"owner_type"]];
+    self.owner = [[NSClassFromString(ownerClass) alloc] init];
+    [self.owner setDbid:properties[@"owner_id"]];
+}
+
+
+@end
