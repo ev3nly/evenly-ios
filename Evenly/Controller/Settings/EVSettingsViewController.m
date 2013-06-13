43d3e6371273810ef22cb2e462bbe0d9a8bf856d
Joe Hankin <joseph.hankin@gmail.com>
2013-06-13 15:35:27 -0700

    Refactored settings view controller using EVFormView

--- Evenly/Controller/Settings/EVSettingsViewController.m
+++ Evenly/Controller/Settings/EVSettingsViewController.m
@@ -12,13 +12,15 @@
 #import "EVSession.h"
 #import "EVNavigationManager.h"
 
+#import "EVFormView.h"
+
 #define EV_SETTINGS_MARGIN 10.0
 #define EV_SETTINGS_ROW_HEIGHT 50.0
 #define EV_SETTINGS_STRIPE_HEIGHT 1.0
 
 @interface EVSettingsViewController ()
 
-@property (nonatomic, strong) UIImageView *panel;
+@property (nonatomic, strong) EVFormView *panel;
 @property (nonatomic, strong) EVSettingsRow *notificationsRow;
 @property (nonatomic, strong) EVSettingsRow *passcodeRow;
 @property (nonatomic, strong) EVSettingsRow *logoutRow;
@@ -39,48 +41,35 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.panel = [[UIImageView alloc] initWithFrame:CGRectMake(EV_SETTINGS_MARGIN,
+    self.panel = [[EVFormView alloc] initWithFrame:CGRectMake(EV_SETTINGS_MARGIN,
                                                                EV_SETTINGS_MARGIN,
                                                                self.view.frame.size.width - 2*EV_SETTINGS_MARGIN,
                                                                EV_SETTINGS_ROW_HEIGHT * 3 + EV_SETTINGS_STRIPE_HEIGHT * 4)];
-    [self.panel setImage:[EVImages resizableTombstoneBackground]];
-    [self.panel setUserInteractionEnabled:YES];
     [self.view addSubview:self.panel];
     
     CGFloat yOrigin = 1.0;
     EVSettingsRow *row = nil;
-    UIView *stripe = nil;
+    CGRect frame = CGRectMake(1, 1, self.panel.frame.size.width-2, EV_SETTINGS_ROW_HEIGHT);
     
-    row = [[EVSettingsRow alloc] initWithFrame:CGRectMake(1, yOrigin, self.panel.frame.size.width-2, EV_SETTINGS_ROW_HEIGHT)];
+    row = [[EVSettingsRow alloc] initWithFrame:frame];
     row.iconView.image = [UIImage imageNamed:@"Settings_notification_globe"];
     row.label.text = @"Notifications";
-    [self.panel addSubview:row];
     self.notificationsRow = row;
     yOrigin += EV_SETTINGS_ROW_HEIGHT;
     
-    stripe = [[UIView alloc] initWithFrame:CGRectMake(0, yOrigin, self.panel.frame.size.width, EV_SETTINGS_STRIPE_HEIGHT)];
-    stripe.backgroundColor = [EVColor newsfeedStripeColor];
-    [self.panel addSubview:stripe];
-    yOrigin += EV_SETTINGS_STRIPE_HEIGHT;
-    
-    row = [[EVSettingsRow alloc] initWithFrame:CGRectMake(1, yOrigin, self.panel.frame.size.width - 2, EV_SETTINGS_ROW_HEIGHT)];
+    row = [[EVSettingsRow alloc] initWithFrame:frame];
     row.iconView.image = [UIImage imageNamed:@"Settings_passcode_key"];
     row.label.text = @"Passcode";
-    [self.panel addSubview:row];
     self.passcodeRow = row;
     yOrigin += EV_SETTINGS_ROW_HEIGHT;
-    
-    stripe = [[UIView alloc] initWithFrame:CGRectMake(0, yOrigin, self.panel.frame.size.width, EV_SETTINGS_STRIPE_HEIGHT)];
-    stripe.backgroundColor = [EVColor newsfeedStripeColor];
-    [self.panel addSubview:stripe];
-    yOrigin += EV_SETTINGS_STRIPE_HEIGHT;
 
-    row = [[EVSettingsRow alloc] initWithFrame:CGRectMake(1, yOrigin, self.panel.frame.size.width - 2, EV_SETTINGS_ROW_HEIGHT)];
+    row = [[EVSettingsRow alloc] initWithFrame:frame];
     row.iconView.image = [UIImage imageNamed:@"Settings_logout_arrow"];
     row.label.text = @"Logout";
-    [self.panel addSubview:row];
     self.logoutRow = row;
     yOrigin += EV_SETTINGS_ROW_HEIGHT;
+    
+    [self.panel setFormRows:@[ self.notificationsRow, self.passcodeRow, self.logoutRow ]];
     
     [self.notificationsRow addTarget:self action:@selector(notificationsButtonPress:) forControlEvents:UIControlEventTouchUpInside];
     [self.passcodeRow addTarget:self action:@selector(passcodeButtonPress:) forControlEvents:UIControlEventTouchUpInside];
