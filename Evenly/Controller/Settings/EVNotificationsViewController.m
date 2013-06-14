04b445657de8c39a30485db550d84c67351bf637
Joe Hankin <joseph.hankin@gmail.com>
2013-06-14 11:51:41 -0700

    Refactored EVNotificationsViewController

--- Evenly/Controller/Settings/EVNotificationsViewController.m
+++ Evenly/Controller/Settings/EVNotificationsViewController.m
@@ -11,10 +11,22 @@
 #import "EVFormView.h"
 #import "EVFormRow.h"
 
+#define CONTEXT_LABEL_X_MARGIN 20.0
+#define CONTEXT_LABEL_Y_MARGIN 15.0
+#define CONTEXT_LABEL_HEIGHT 20.0
+
+#define FORM_Y_ORIGIN 44.0
+#define FORM_MARGIN 10.0
+#define FORM_ROW_HEIGHT 50.0
+
 @interface EVNotificationsViewController ()
 
 @property (nonatomic, strong) UILabel *contextLabel;
 @property (nonatomic, strong) EVFormView *form;
+
+- (void)loadContextLabel;
+- (void)loadForm;
+- (void)loadRows;
 
 @end
 
@@ -33,18 +45,34 @@
 {
     [super viewDidLoad];
     
-    self.contextLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 15, self.view.frame.size.width - 40, 20)];
+    [self loadContextLabel];
+    [self loadForm];
+    [self loadRows];
+}
+
+- (void)loadContextLabel {
+    self.contextLabel = [[UILabel alloc] initWithFrame:CGRectMake(CONTEXT_LABEL_X_MARGIN,
+                                                                  CONTEXT_LABEL_Y_MARGIN,
+                                                                  self.view.frame.size.width - 2*CONTEXT_LABEL_X_MARGIN,
+                                                                  CONTEXT_LABEL_HEIGHT)];
     self.contextLabel.backgroundColor = [UIColor clearColor];
     self.contextLabel.textColor = [EVColor inputTextColor];
     self.contextLabel.font = [EVFont blackFontOfSize:15];
     self.contextLabel.text = @"Send me notifications via...";
     [self.view addSubview:self.contextLabel];
-    
-    self.form = [[EVFormView alloc] initWithFrame:CGRectMake(10, 44.0, self.view.frame.size.width - 20, 150)];
+}
+
+- (void)loadForm {
+    self.form = [[EVFormView alloc] initWithFrame:CGRectMake(FORM_MARGIN,
+                                                             FORM_Y_ORIGIN,
+                                                             self.view.frame.size.width - 2*FORM_MARGIN,
+                                                             3*FORM_ROW_HEIGHT)];
     [self.view addSubview:self.form];
-    
+}
+
+- (void)loadRows {
     EVFormRow *row = nil;
-    CGRect rect = CGRectMake(0, 0, self.form.frame.size.width, 50.0);
+    CGRect rect = CGRectMake(0, 0, self.form.frame.size.width, FORM_ROW_HEIGHT);
     NSMutableArray *array = [NSMutableArray array];
     
     row = [[EVFormRow alloc] initWithFrame:rect];
