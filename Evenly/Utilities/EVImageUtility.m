e48fe84ef32c73da6a7cff0056bfeac995e551ec
Joe Hankin <joseph.hankin@gmail.com>
2013-07-15 14:48:04 -0700

    Removed a little cruft

--- Evenly/Utilities/EVImageUtility.m
+++ Evenly/Utilities/EVImageUtility.m
@@ -58,22 +58,6 @@
     UIImage *screenImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return screenImage;
-
-//    CGRect screenRect = [[UIScreen mainScreen] bounds];
-//    
-//    UIGraphicsBeginImageContext(screenRect.size);
-//    
-//    CGContextRef ctx = UIGraphicsGetCurrentContext();
-//    [[UIColor blackColor] set];
-//    CGContextFillRect(ctx, screenRect);
-//    
-//    [view.layer renderInContext:ctx];
-//    
-//    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-//    
-//    UIGraphicsEndImageContext();
-//    
-//    return newImage;
 }
 
 
