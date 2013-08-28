73098c18887841714295d1b2d30fc52b7118d015
Joe Hankin <joseph.hankin@gmail.com>
2013-08-27 18:13:32 -0700

    One more goddamn refactoring

--- Evenly/View/TableViewCells/EVMainMenuCell.m
+++ Evenly/View/TableViewCells/EVMainMenuCell.m
@@ -60,15 +60,26 @@
     [super layoutSubviews];
     
     [self.label sizeToFit];
-    [self.label setFrame:CGRectMake(CGRectGetMaxX(self.iconView.frame),
-                                    (self.contentView.frame.size.height - self.label.frame.size.height) / 2.0,
-                                    self.label.frame.size.width,
-                                    self.label.frame.size.height)];
+    [self.label setFrame:[self labelFrame]];
     
     if (self.marketingView) {
-        [self.marketingView setFrame:CGRectMake(CGRectGetMaxX(self.label.frame), 0, self.contentView.frame.size.width - CGRectGetMaxX(self.label.frame), self.contentView.frame.size.height)];
+        [self.marketingView setFrame:[self marketingViewFrame]];
         [self.contentView addSubview:self.marketingView];
     }
+}
+
+- (CGRect)labelFrame {
+    return CGRectMake(CGRectGetMaxX(self.iconView.frame),
+                      (self.contentView.frame.size.height - self.label.frame.size.height) / 2.0,
+                      self.label.frame.size.width,
+                      self.label.frame.size.height);
+}
+
+- (CGRect)marketingViewFrame {
+    return CGRectMake(CGRectGetMaxX(self.label.frame),
+                      0,
+                      self.contentView.frame.size.width - CGRectGetMaxX(self.label.frame),
+                      self.contentView.frame.size.height);
 }
 
 @end
