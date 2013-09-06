9a5ac8d6ec0f67fb4b9bc5914591663f4bc324c4
Joe Hankin <joseph.hankin@gmail.com>
2013-09-06 13:06:53 -0700

    Crashlytics #114: Prevent NSDecimalNumberOverflowException
    Per Crashlytics #114, we're seeing some weird NSDecimalNumberOverflowExceptions being raised
    in `[EVRewardsGameViewController updateBalance]` when the addition takes place.
    I have no idea why, considering we're never even coming close to overflowing NSDecimalNumber.
    In any case, my solution is to catch the exception and, instead of doing the addition locally,
    just reload Me to update the balance.

--- Evenly/Controller/Rewards/EVRewardsGameViewController.m
+++ Evenly/Controller/Rewards/EVRewardsGameViewController.m
@@ -449,9 +449,19 @@
 - (void)updateBalance {
     NSDecimalNumber *myBalance = [[EVCIA me] balance];
     NSDecimalNumber *rewardAmount = self.reward.selectedAmount;
-    NSDecimalNumber *newBalance = [myBalance decimalNumberByAdding:rewardAmount];
-    [self.balanceView.balanceLabel setText:[EVStringUtility amountStringForAmount:newBalance]];
-    [[[EVCIA sharedInstance] me] setBalance:newBalance];
+
+    // JH: Per Crashlytics #114, we're seeing some weird NSDecimalNumberOverflowExceptions being raised
+    // in this method when the addition takes place.  I have no idea why, considering we're never even coming
+    // close to overflowing NSDecimalNumber.  In any case, my solution is to catch the exception and,
+    // instead of doing the addition locally, just reload Me to update the balance.
+    @try {
+        NSDecimalNumber *newBalance = [myBalance decimalNumberByAdding:rewardAmount];
+        [self.balanceView.balanceLabel setText:[EVStringUtility amountStringForAmount:newBalance]];
+        [[[EVCIA sharedInstance] me] setBalance:newBalance];
+    }
+    @catch (NSException *exception) {
+        [EVCIA reloadMe];
+    }
 }
 
 - (void)switchTopLabel {
